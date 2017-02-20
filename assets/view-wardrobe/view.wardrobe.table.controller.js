db43c35c990faa074b3c9c679f72b074056fe559
yexiaorain <yexiaorain@gmail.com>
2017-02-20 15:35:50 +0800

    filter finish

--- assets/view-wardrobe/view.wardrobe.table.controller.js
+++ assets/view-wardrobe/view.wardrobe.table.controller.js
@@ -3,7 +3,8 @@
 
   angular
     .module('nikki.view.wardrobe-table')
-    .controller('ViewWardrobeTableController', ViewWardrobeTableController);
+    .controller('ViewWardrobeTableController', ViewWardrobeTableController)
+    .filter('ViewWardrobeTableCustomFilter',ViewWardrobeTableCustomFilter);
 
   ViewWardrobeTableController.$inject = ['$scope', '$rootScope'];
   function ViewWardrobeTableController($scope, $rootScope) {
@@ -14,6 +15,23 @@
         return {"":-1,"C":0,"B":1,"A":2,"S":3,"SS":4}[row[o]];
       }
     }
-    
+  }
+
+  ViewWardrobeTableCustomFilter.$inject = ['$filter'];
+  function ViewWardrobeTableCustomFilter($filter){
+    return function(input, predicate){
+      var i , l = input.length, ret = [];
+      for(i = 0; i < l; i++) {
+        var isInclude = true;
+        _.forEach(predicate,function(val,key){
+          var arri = key.slice(1);
+          if(!_.includes(input[i][arri],val))
+            isInclude = false;
+        });
+        if(isInclude)
+          ret.push(input[i]);
+      }
+      return ret;
+    }
   }
 })();
