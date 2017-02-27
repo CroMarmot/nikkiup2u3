97286f16504de26c054e9032bec5041c61b59635
yexiaorain <yexiaorain@gmail.com>
2017-02-27 16:15:40 +0800

    finish append function

--- assets/wardrobe-mine/wardrobe.mine.controller.js
+++ assets/wardrobe-mine/wardrobe.mine.controller.js
@@ -37,15 +37,46 @@
       $scope.category = $rootScope.categoryMain;    
     });
 
+    /*
+     * wardrobe_base one category
+     * wardrobe_str input like 001,2,003,4,5,6,1234
+     * */
+    function append2wardrobe(wardrobe_base,wardrobe_str){
+      var appendwardrobe = [];
+      _.forEach(wardrobe_str.split(","),function(value){
+        if(value.length > 3)
+          appendwardrobe.push(value);
+        else if(value.length > 0)
+          appendwardrobe.push(("000"+value).slice(-3));
+      });
+      return _.union(wardrobe_base,appendwardrobe);
+    }
+
     $scope.appendClick = function(){
       //[TODO] use flash to show success/error message
       try{
-        //[TODO] finish add function
-        //var loadwardrobe = JSON.parse($scope.view.syn);
-        //$rootScope.mineWardrobe = loadwardrobe;
-        //DataService.SetMineWardrobe(loadwardrobe);
+        var loadwardrobe = $rootScope.mineWardrobe;
+        var appendwardrobe = $scope.view.append;
+        appendwardrobe = _.replace(appendwardrobe," ","");//remove all space
+        if(_.isUndefined(loadwardrobe))
+          loadwardrobe = {};
+        // compatibility for 上衣:001,002|下装:234,567
+        if(_.isEmpty($scope.data.category)){
+          var categorys = appendwardrobe.split("|");
+          _.forEach(categorys,function(v){
+            var parts = v.split(":");
+            if(parts.length == 2){
+              var cg=parts[0],apw=parts[1];//category and append wardrobe
+              loadwardrobe[cg]=append2wardrobe(loadwardrobe[cg],apw);
+            }
+          });
+        }else{
+          loadwardrobe[$scope.data.category]=append2wardrobe(loadwardrobe[$scope.data.category],appendwardrobe);
+        }
+        $rootScope.mineWardrobe = loadwardrobe;
+        DataService.SetMineWardrobe(loadwardrobe);
       }catch(err){
-        console.log(err.message);      
+        console.dir(err.message);
       }
     };
     
